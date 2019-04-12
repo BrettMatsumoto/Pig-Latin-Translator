b3f2b1723d316f0867421bd3da2d466f945ac721
Brett <brett_matsumoto@yahoo.com>
2019-04-12 09:57:48 -1000

    reformatted using prettier

--- js/app.js
+++ js/app.js
@@ -7,11 +7,13 @@ function toPigLatin(str) {
 
   if (firstChar === 'a' || firstChar === 'e' || firstChar === 'i' || firstChar === 'o' || firstChar === 'u') {
     let result = prep + '-ay';
+
     return result;
   } else {
     for (let i = 1; i !== 'a' || i !== 'e' || i !== 'i' || i !== 'o' || i !== 'u' || i !== 'y'; i++) {
       let appended = '-' + prep.slice(0, i) + 'ay';
       let notAppended = prep.substring(i, prep.length);
+
       return notAppended + appended;
     }
   }
